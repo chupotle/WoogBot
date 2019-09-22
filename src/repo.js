d430729ac7a6c28b3d4b0db1a0af3ecc3105fed2
Tim <chupotle@users.noreply.github.com>
2019-09-22 02:18:25 -0400

    🎺🍎 Updated with Glitch (#5)

--- src/repo.js
+++ src/repo.js
@@ -10,9 +10,10 @@ function listRoles()  {
   return roles;
 };
 
-function checkRole(role_name)  {
+function roleExists(role_name)  {
   const sql = db.prepare(`SELECT role_name FROM roles WHERE role_name = ?`);
   const res = sql.get(role_name);
+  console.log(!!res);
   return res;
 };
 
@@ -26,7 +27,10 @@ function deleteRole(role_name) {
   const sql = db.prepare(`DELETE FROM 'roles' 
     WHERE role_name = ?`);
   const res = sql.run(role_name);
-  return res;
+  const sql2 = db.prepare(`DELETE FROM 'users' 
+    WHERE role_name = ?`);
+  const res2 = sql2.run(role_name);
+  return [res,res2];
 };
 
 function giveRole(user_id, role_name) {
@@ -43,7 +47,7 @@ function removeRole(user_id, role_name) {
   return res;
 };
 
-const getUsersWithRole = (role_name) => {  
+function getUsersWithRole(role_name) {  
   const sql = db.prepare(`SELECT DISTINCT user_id FROM 'users' WHERE role_name = ?`);
   var user_ids = [];
   for (const user of sql.iterate(role_name)) {
@@ -52,4 +56,13 @@ const getUsersWithRole = (role_name) => {
   return user_ids;
 };
 
-module.exports = { listRoles, addRole, giveRole, removeRole, getUsersWithRole, checkRole, deleteRole };
+function myRoles(user_id) {  
+  const sql = db.prepare(`SELECT DISTINCT role_name FROM 'users' WHERE user_id = ?`);
+  var role_names = [];
+  for (const user of sql.iterate(user_id)) {
+    role_names.push(user.role_name);
+  }
+  return role_names;
+};
+
+module.exports = { listRoles, addRole, giveRole, removeRole, getUsersWithRole, roleExists, deleteRole, myRoles };
